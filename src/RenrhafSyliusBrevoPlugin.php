088a5c91339a493dd6c2f2e4edfcae6d8f924959
Quentin Fahrner <fahrner.quentin@gmail.com>
2026-04-05 12:51:16 +0200

    fix: override getContainerExtension() to match bundle name to extension alias

--- src/RenrhafSyliusBrevoPlugin.php
+++ src/RenrhafSyliusBrevoPlugin.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Renrhaf\SyliusBrevoPlugin;
 
+use Renrhaf\SyliusBrevoPlugin\DependencyInjection\RenrhafSyliusBrevoExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class RenrhafSyliusBrevoPlugin extends Bundle
@@ -11,5 +13,10 @@ final class RenrhafSyliusBrevoPlugin extends Bundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function getContainerExtension(): ExtensionInterface
+    {
+        return new RenrhafSyliusBrevoExtension();
     }
 }
